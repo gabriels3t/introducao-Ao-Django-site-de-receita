29fb451752ca15cbea784806f3b2f29fea576918
gabriels3t <gabriels3t@hotmail.com>
2021-01-10 13:22:22 -0300

    admin,parametros e receitas

--- receitas/urls.py
+++ receitas/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns =[
     path('',views.index,name='index'),
-    path('receita',views.receita,name='receita')
+    path('<int:receita_id>',views.receita,name='receita')
 ]
 #  path('nome que eu quero da url',views.Pagina q eu quero chamar
 # ,name='nomedapaginaigual do html')
+
+# <int:receita_id> Para passar parametro exemplo receita?id=1

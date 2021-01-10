c47a75ee000c03d5acbd6d96b89a817558ea0859
gabriels3t <gabriels3t@hotmail.com>
2021-01-10 14:36:05 -0300

    integrando nome das pessoas com as receitas

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'receitas'
+    'receitas',
+    'pessoas',
 ]
 
 MIDDLEWARE = [

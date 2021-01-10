6bcde2d53acdb8b49777a70de50a41d393c09aa0
gabriels3t <gabriels3t@hotmail.com>
2021-01-10 01:48:59 -0300

    Modelo e banco de dados

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1:'lasanha',
+        2:'Sopa de legumes',
+        3:'Sorvete',
+        4:'Bolo de chocolate'
+    }
+
+    dados = {
+        'nome_da_receita':receitas
+    }
+    return render(request,'index.html',dados)
 
 def receita(request):
     return render(request,'receita.html')

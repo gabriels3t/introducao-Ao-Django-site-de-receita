bad1966281aa30208846ed3d170cafa3723f80b7
gabriels3t <gabriels3t@hotmail.com>
2021-01-10 00:32:57 -0300

    links, extends e partials

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('receita',views.receita,name='receita')
 ]
+#  path('nome que eu quero da url',views.Pagina q eu quero chamar
+# ,name='nomedapaginaigual do html')

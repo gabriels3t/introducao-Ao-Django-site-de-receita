6e5d17da199b1c537fb6dcf5e51315505d052d2f
gabriels3t <gabriels3t@hotmail.com>
2021-01-13 11:47:42 -0300

    Paginaçao no Django

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404, redirec
 from receitas.models import Receita
 from django.contrib.auth.models import User
 from django.contrib import auth,messages
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 
 # Create your views here.
 
 def index(request):
     receitas = Receita.objects.order_by('-data_receita').filter(publicada = True)
+    # Paginação do site
+    paginator = Paginator(receitas,3) # Paginator(Dado,numero de dados por pagina)
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
     dados = {
-        'receitas':receitas
+        'receitas':receitas_por_pagina
     }
     return render(request,'receitas/index.html',dados)
 

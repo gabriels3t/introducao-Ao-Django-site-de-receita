bc1ef30fbfc6c2ac01d9fc76b3010579bef11449
gabriels3t <gabriels3t@hotmail.com>
2021-01-13 11:19:20 -0300

    Melhorando a arquitetur

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns =[
-    path('',views.index,name='index'),
-    path('<int:receita_id>',views.receita,name='receita'),
-    path('buscar',views.buscar,name='buscar')
+    path('',index,name='index'),
+    path('<int:receita_id>',receita,name='receita'),
+    path('buscar',buscar,name='buscar'), 
+    path('cria/receita',cria_receita,name='cria_receita'),
+    path('deleta/<int:receita_id>',deleta_receita,name='deleta_receita'),
+    path('editar/<int:receita_id>',editar_receita,name='edita_receita'),
+    path('atualiza_receita',atualiza_receita,name='atualiza_receita'),
 ]
-#  path('nome que eu quero da url',views.Pagina q eu quero chamar
+
+#  path('nome que eu quero da url',Pagina q eu quero chamar
 # ,name='nomedapaginaigual do html')
 
 # <int:receita_id> Para passar parametro exemplo receita?id=1

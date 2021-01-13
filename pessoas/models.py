6dd94a6e9bcb605a47b70fcc39cbcec278f5c0f0
gabriels3t <gabriels3t@hotmail.com>
2021-01-13 11:58:03 -0300

    Finalizando o projeto

--- pessoas/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.db.models.fields import CharField, EmailField
-
-class Pessoa(models.Model):
-    nome = CharField(max_length=200)
-    email = CharField(max_length=200)
-
-    def __str__(self):
-        return self.nome 
